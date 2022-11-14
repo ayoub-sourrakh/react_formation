dd7f5823ccfb0efba0fa9dbc355e8be6c1f9ddfd
Ayoub Sourrakh <ayoub.sourrakh@outlook.com>
2022-11-14 17:35:42 +0100

    fin de journée

--- react_formation_s2/src/components/Tasks/Task.jsx
+++ react_formation_s2/src/components/Tasks/Task.jsx
@@ -1,14 +1,11 @@
 import React from 'react'
 import Menu from './Menu'
 
-const Task = ({task}) => {
-
-  return (
+const Task = ({task}) => (
     <div>
         <h3>{task.task}</h3>
         <Menu />
     </div>
-  )
-}
+)
 
 export default Task
